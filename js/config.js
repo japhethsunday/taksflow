35599b33837797b25823b549cf38e6833236c113
User <user@example.com>
2026-09-12 17:54:32 -0700

    Add config.js directly, remove build command, disable .gitignore for config

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Generated at build time — do not edit.
+window.CONFIG = {
+  API_BASE: "/api",
+  PROJECT_ID: "46c57c7a-efe2-4ef0-947c-758c9e850b85",
+  API_KEY: "cn_agent_bXjGBsQ3Wb3MFYHikhv1Cra0FRXdqTT4L2JbfiME4J4",
+};
